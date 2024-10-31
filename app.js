6d26463b46d5f62d5ff93328066022f1537763aa
Smyrn96 <manosmyrnakis@hotmail.com>
2024-10-31 18:52:59 +0200

    fix: fix the file structure

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
+app.use(morgan('dev'));
+app.use(express.json());
+app.use('/api/v1/tours/', tourRouter);
+app.use('/api/v1/users/', userRouter);
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
-});
-
-const port = 3000;
-app.listen(port, () => {
-  console.log('listening on port ', port);
-});
+module.exports = app;
