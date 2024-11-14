5e69b38651cb35ab16dc04e4343f7d905d94613e
Smyrn96 <manosmyrnakis@hotmail.com>
2024-11-14 15:43:17 +0200

    feat: Uncaught exceptions and unhandled

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNCAUGHT EXCEPTION...SHUTTING DOWN');
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
@@ -15,7 +21,16 @@ mongoose.connect(DB).then((con) => {
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log('listening on port ', port);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHADLED REJECTION...SHUTTING DOWN');
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
